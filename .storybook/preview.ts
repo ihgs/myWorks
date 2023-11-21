407738ce8e29d0dce690af7ebf299e5f84aa2310
ihgs <hgs.dev@gmail.com>
2023-11-21 10:22:47 +0900

    storybook settings for mui

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,4 +1,7 @@
 import type { Preview } from "@storybook/react";
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import { withThemeFromJSXProvider } from '@storybook/addon-themes';
+import { darkMode, lightMode}  from "../src/app/themes";
 
 const preview: Preview = {
   parameters: {
@@ -11,5 +14,17 @@ const preview: Preview = {
     },
   },
 };
+
+
+export const decorators = [
+  withThemeFromJSXProvider({
+    themes: {
+      dark: darkMode,
+      light: lightMode
+    },
+    defaultTheme: 'light',
+    Provider: ThemeProvider,
+    GlobalStyles: CssBaseline,
+})];
 
 export default preview;
---
+++ src/app/themes.ts
@@ -0,0 +1,17 @@
+import { createTheme } from "@mui/material";
+
+export const darkMode = createTheme(
+    {
+        palette: {
+            mode: 'dark',
+          },    
+    }
+)
+
+export const lightMode = createTheme(
+    {
+        palette: {
+            mode: 'light',
+          },    
+    }
+)
